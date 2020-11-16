1dc0cb5e4a634e62931627d78ef0559883d8c498
EricHier <developer.erichier@gmail.com>
2020-11-16 20:10:46 +0100

    created spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="lds-dual-ring"></div>
+</template>
+<style scoped lang="scss">
+$accent: white;
+
+.lds-dual-ring {
+  display: block;
+  width: 2rem;
+  height: 2rem;
+}
+
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin: 0.25rem;
+  border-radius: 50%;
+  border: 3px solid $accent;
+  border-color: $accent transparent $accent transparent;
+  animation: lds-dual-ring 1s linear infinite;
+}
+
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
