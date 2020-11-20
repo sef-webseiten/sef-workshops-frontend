27320b31755c0b43d0fa7068b0d7dcaf5d2b1d24
EricHier <developer.erichier@gmail.com>
2020-11-21 00:21:30 +0100

    implemented firebase emulator

--- src/firebase-client.js
+++ src/firebase-client.js
@@ -1,15 +1,14 @@
 import firebase from "firebase/app";
-import "firebase/firestore";
 import "firebase/auth";
+import "firebase/functions"
 import {firebaseConfig} from "./firebase-api";
 import * as firebaseui from "firebaseui";
 
 export const app = firebase.initializeApp(firebaseConfig);
 
-export const firestore = app.firestore();
+const emulator = location.host.match(/localhost:\d*/g);
 
 export const uiConfig = {
-    signInSuccessUrl: '/',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -26,7 +25,20 @@ export const uiConfig = {
     }
 };
 
+if(!emulator)
+    uiConfig.signInSuccessUrl = '/';
+
 export const auth = app.auth();
+
+if (emulator) {
+    auth.useEmulator("http://localhost:9099/")
+    auth.signInWithEmailAndPassword("eric@gmail.com", "123456").then(console.log).catch(console.log)
+}
 
 // Initialize the FirebaseUI Widget using Firebase.
 export const ui = new firebaseui.auth.AuthUI(auth);
+
+export const functions = app.functions("europe-west3");
+
+if(emulator)
+    functions.useFunctionsEmulator("http://localhost:5001");
