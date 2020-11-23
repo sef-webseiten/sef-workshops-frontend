eafbe91abbbb521648067f7cd93237d28b79b3bd
EricHier <developer.erichier@gmail.com>
2020-11-23 23:57:42 +0100

    moved firebase credentials to env

---
+++ src/firebase-api.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: process.env.GRIDSOME_FIREBASE_API_KEY,
+    authDomain: process.env.GRIDSOME_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.GRIDSOME_FIREBASE_DATABASE_URL,
+    projectId: process.env.GRIDSOME_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.GRIDSOME_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.GRIDSOME_FIREBASE_MESSAGING_SENDER,
+    appId: process.env.GRIDSOME_FIREBASE_APP_ID,
+};
