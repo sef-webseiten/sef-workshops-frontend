2515ac0531528dc95178a9094d96544c73b8f414
EricHier <developer.erichier@gmail.com>
2020-12-24 14:25:05 +0100

    changed navbar popup animation

--- src/components/navigation/NavbarPopup.vue
+++ src/components/navigation/NavbarPopup.vue
@@ -1,7 +1,7 @@
 <template>
   <transition
-      enter-active-class="animate__animated animate__faster animate__bounceInRight"
-      leave-active-class="animate__animated animate__faster animate__bounceOutRight"
+      enter-active-class="animate__animated animate__faster animate__fadeInRight"
+      leave-active-class="animate__animated animate__faster animate__fadeOutRight"
   >
     <div class="absolute z-30 right-0">
       <div class="z-30 absolute right-0 bg-white w-4 h-4 transform rotate-45 translate-y-1/2 mr-12"></div>
