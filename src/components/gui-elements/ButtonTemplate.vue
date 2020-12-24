735de6a6453fe5a3f1154400fec2fe69877274d8
EricHier <developer.erichier@gmail.com>
2020-12-24 12:10:52 +0100

    added a button and heading template

---
+++ src/components/gui-elements/ButtonTemplate.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="px-2 py-2 text-white bg-primary rounded-full w-full">
+    {{ content }}
+  </button>
+</template>
+<script>
+export default {
+  props: ["content"]
+}
+</script>
