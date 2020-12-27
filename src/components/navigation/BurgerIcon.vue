027c7006bb42f100845dabadf6edb13f337e26a2
EricHier <developer.erichier@gmail.com>
2020-12-27 22:42:48 +0100

    improved BurgerIcon.vue

--- src/components/navigation/BurgerIcon.vue
+++ src/components/navigation/BurgerIcon.vue
@@ -1,17 +1,19 @@
 <template>
   <button @click="$emit('click')">
-    <span class="line"/>
+    <span class="line mt-1"/>
+    <span class="line space"/>
     <span class="line space"/>
     <span class="line space"/>
   </button>
 </template>
 <style scoped lang="scss">
 .line {
-  height: 0.2rem;
-  @apply w-8 bg-white rounded-full block;
+  height: 0.125rem;
+  width: 1.75rem;
+  @apply bg-white rounded-full block;
 }
 
 .space {
-  @apply mt-2;
+  margin-top: 0.25rem;
 }
 </style>
