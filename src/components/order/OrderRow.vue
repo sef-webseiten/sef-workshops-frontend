90f9c00bfefe8343fbec9734da5c8bec72c4963f
EricHier <developer.erichier@gmail.com>
2020-12-31 20:19:00 +0100

    fixed order page on mobile

--- src/components/order/OrderRow.vue
+++ src/components/order/OrderRow.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="rounded shadow-2xl mb-6 last:mb-0 p-6 md:grid grid-cols-3 gap-6">
-    <g-link :to="`/w/${order.workshop._id}`" class="box p-2">
+  <div class="rounded shadow-2xl mb-6 last:mb-0 p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+    <g-link :to="`/w/${order.workshop._id}`" class="box block p-2">
       <div class="mb-4">Workshop: {{order.workshop.title}} </div>
       <div v-for="date in order.event.dates">
         {{date.timeString}}
       </div>
     </g-link>
-    <div class="box p-2">
+    <div class="box block p-2">
       Betrag: {{order.price}} EURO <br>
       Datum: {{order.timestamp}}
     </div>
