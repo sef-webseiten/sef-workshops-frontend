bb45bdc2b70ed3ee374a607c71fa8f7b84538f93
Ondolin <developer@marvin-kaiser.eu>
2021-01-20 21:38:57 +0100

    Added close animation for payment buttons

--- src/components/PaymentButtons.vue
+++ src/components/PaymentButtons.vue
@@ -1,79 +1,86 @@
 <template>
   <div class="fixed bottom-0 right-0 w-screen md:w-1/2 lg:w-1/3 select-none z-30">
-    <div v-if="visible"
-         class="relative bg-white rounded-t p-6 md:mx-6 shadow-2xl animate__animated animate__slideInUp">
+    <transition
+        enter-active-class="animate__animated animate__slideInUp"
+        leave-active-class="animate__animated animate__slideOutDown"
+    >
+      <div v-if="visible"
+           class="relative bg-white rounded-t p-6 md:mx-6 shadow-2xl">
 
-      <div class="group absolute right-0 top-0 -mt-14 p-6 pb-2" @click="resetPaymentButton">
-        <fa class="group-hover:bounce" :icon="['fas', 'chevron-down']"/>
-      </div>
+        <div class="group absolute right-0 top-0 -mt-14 p-6 pb-2" @click="resetPaymentButton">
+          <fa class="group-hover:bounce" :icon="['fas', 'chevron-down']"/>
+        </div>
 
-      <div class="between mb-4">
-        <p>Teilnehmer</p>
-        <div>
+        <div class="between mb-4">
+          <p>Teilnehmer</p>
+          <div>
 
-          <!-- minus icon -->
-          <div v-if="step === 1" v-show="participants > 1" class="p-1 inline-block" @click="participants--">
-            <fa :icon="['fas', 'minus']"/>
-          </div>
+            <!-- minus icon -->
+            <div v-if="step === 1" v-show="participants > 1" class="p-1 inline-block" @click="participants--">
+              <fa :icon="['fas', 'minus']"/>
+            </div>
 
-          <!-- number -->
-          <span class="mx-2 font-medium text-lg">
+            <!-- number -->
+            <span class="mx-2 font-medium text-lg">
             {{ participants }}
           </span>
 
-          <!-- plus icon -->
-          <div v-if="step === 1" class="p-1 inline-block" @click="participants++">
-            <fa :icon="['fas', 'plus']"/>
+            <!-- plus icon -->
+            <div v-if="step === 1" class="p-1 inline-block" @click="participants++">
+              <fa :icon="['fas', 'plus']"/>
+            </div>
+
           </div>
-
         </div>
+
+        <!-- price -->
+        <div class="between mb-4">
+          <p>Gesamtbetrag </p>
+          <p class="font-bold text-right text-lg">{{ event.price * participants }} EUR</p>
+        </div>
+
+        <!-- workshop name -->
+        <div class="between mb-4">
+          <p>Workshop</p>
+          <p class="font-bold text-right">{{ workshop.title }}</p>
+        </div>
+
+        <!-- date -->
+        <div class="between-start mb-4">
+          <p>Wann?</p>
+          <p><span v-for="(date, index) in event.dates" :key="index" class="block">{{ date.timeString }}</span></p>
+        </div>
+
+        <!-- next button -->
+        <button
+            v-if="[1,2].includes(step)"
+            :disabled="step === 2"
+            class="w-full bg-primary p-2 font-bold text-white active:bg-light disabled:bg-gray-400 disabled:cursor-default rounded-sm"
+            @click="step++">
+          <span v-show="step === 1">Weiter</span>
+          <Spinner v-show="step === 2" class="mx-auto"/>
+        </button>
+
+        <!-- paypal buttons -->
+        <div v-show="step === 3" id="paypal-button-container"></div>
+
+        <div class="text-blue-800 text-center mt-2">
+          <g-link target="_blank" title="AGB" to="/agb">AGB</g-link>
+          ·
+          <g-link target="_blank" title="Datenschutzerklärung" to="/datenschutzerklaerung">Datenschutzerklärung</g-link>
+        </div>
+
       </div>
-
-      <!-- price -->
-      <div class="between mb-4">
-        <p>Gesamtbetrag </p>
-        <p class="font-bold text-right text-lg">{{ event.price * participants }} EUR</p>
-      </div>
-
-      <!-- workshop name -->
-      <div class="between mb-4">
-        <p>Workshop</p>
-        <p class="font-bold text-right">{{ workshop.title }}</p>
-      </div>
-
-      <!-- date -->
-      <div class="between-start mb-4">
-        <p>Wann?</p>
-        <p><span v-for="(date, index) in event.dates" :key="index" class="block">{{ date.timeString }}</span></p>
-      </div>
-
-      <!-- next button -->
-      <button
-          v-if="[1,2].includes(step)"
-          :disabled="step === 2" class="w-full bg-primary p-2 font-bold text-white active:bg-light disabled:bg-gray-400 disabled:cursor-default rounded-sm" @click="step++">
-        <span v-show="step === 1">Weiter</span>
-        <Spinner v-show="step === 2" class="mx-auto"/>
-      </button>
-
-      <!-- paypal buttons -->
-      <div v-show="step === 3" id="paypal-button-container"></div>
-
-      <div class="text-blue-800 text-center mt-2">
-        <g-link target="_blank" title="AGB" to="/agb">AGB</g-link>
-        ·
-        <g-link target="_blank" title="Datenschutzerklärung" to="/datenschutzerklaerung">Datenschutzerklärung</g-link>
-      </div>
-
-    </div>
+    </transition>
   </div>
 </template>
 <script>
 import Default from "../layouts/Default";
 import Spinner from "./gui-elements/Spinner";
-import { getAffiliateCode } from "../plugins/affiliate";
+import {getAffiliateCode} from "../plugins/affiliate";
 
 export default {
-  components: { Spinner, Default },
+  components: {Spinner, Default},
   data() {
     return {
       visible: false,
@@ -123,7 +130,7 @@ export default {
             eventID: this.event._id,
             affiliate: getAffiliateCode()
           }
-      ).then(({ createOrder: orderNumber }) => this.setupPaypalButtons(orderNumber));
+      ).then(({createOrder: orderNumber}) => this.setupPaypalButtons(orderNumber));
     },
     setupPaypalButtons(orderID) {
 
