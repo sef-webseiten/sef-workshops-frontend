852cc77ac57b92ac021855e081e7e5d2787202ff
EricHier <developer.erichier@gmail.com>
2021-02-20 14:10:17 +0100

    fixed trainer image sizing on medium devices

--- src/components/landing-page/TrainerImage.vue
+++ src/components/landing-page/TrainerImage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-10">
     <g-image src="~/assets/img/abi/frau.jpg" width="800"
-             class="w-full h-auto md:h-96 md:w-96 mb-10 ml-auto rounded-full shadow-2xl"
+             class="w-full h-auto md:w-96 mb-10 ml-auto rounded-full shadow-2xl"
              draggable="false"/>
   </div>
-</template>
+</template>1
