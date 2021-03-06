32bc23773b078791fb7a9544fafad26f60ef3d54
EricHier <developer.erichier@gmail.com>
2021-03-06 13:33:23 +0100

    adding agb content

--- src/pages/agb.vue
+++ src/pages/agb.vue
@@ -1,6 +1,165 @@
 <template>
   <Layout>
-    <h1 class="h1">AGB</h1>
+    <h1>AGB</h1>
+    <h2>§1 Leistungsbeschreibung</h2>
+    <ol>
+      <li>
+        Eric Feltgen bietet die Plattform of.courz, mittels welcher Anbieter ("Kursgeber") von Live-Kursen, Seminaren,
+        Workshops, Schulungen oder Lehrgängen ("Kursen") diese anbieten und sie für Käufer ("Teilnehmer") auf der
+        Plattform zur Buchung freigeben können.
+      </li>
+      <li>
+        Teilnehmer können Kurse nach verschiedenen, durch den Kursgeber zur Verfügung gestellten, Kriterien und
+        Merkmalen auswählen.
+      </li>
+      <li>
+        Die konkrete Instanz eines Kurses ("Veranstaltung") beinhaltet einen Preis, einen öffentlichen Ort, eine
+        maximale Teilnehmerzahl und eine Menge von Terminen, die Datum und Uhrzeiten beinhalten.
+      </li>
+      <li>
+        Die Teilnehmer können aus den vom Kursgeber eingetragenen Veranstaltungen wählen und sich verbindlich zu
+        entsprechenden Veranstaltungen anmelden.
+      </li>
+      <li>
+        Kursgeber können des Weiteren Informationen zu den Buchungen ihrer Kurse einsehen.
+      </li>
+    </ol>
+
+    <h2>§2 Vertragsschluss</h2>
+
+    <ol>
+
+      <li>
+        Der Kaufvertrag, bei dem Kauf der Teilnahme an einer Veranstaltung, entsteht zwischen dem Kursgeber und dem
+        Teilnehmer. Die Plattform dient lediglich als Unterstützung für den Kursgeber beim Abschluss und der Abwicklung
+        des Kaufes.
+      </li>
+
+      <li>
+        Mit dem inserieren einer Veranstaltung seitens des Kursgebers, macht er den Teilnehmern ein verbindliches
+        Angebot.
+      </li>
+
+      <li>
+        Durch das Klicken des "Jetzt kaufen"-Buttons nimmt der Teilnehmer das Angebot verbindlich an.
+      </li>
+
+      <li>
+        Der Kaufvertrag kommt nur dann zu Stande, wenn dem Teilnehmer durch die Plattform eine Buchungsbestätigung
+        zugeht, um eine Überbuchung einer Veranstaltung zu verhindern. Der Kursgeber ist für die umsatzbare maximale
+        Teilnehmerzahl seiner Veranstaltungen verantwortlich.
+      </li>
+
+      <li>
+        Mit dem gültigen Kauf der Teilnahme an einer Veranstaltung wird dem Teilnehmer das Recht gewährt aktiv an der
+        Veranstaltung teilzunehmen. Der Kursgeber muss alle in seiner Macht stehenden Maßnahmen treffen, um dem
+        Teilnehmer den Zugang zu der Veranstaltung zu ermöglichen.
+      </li>
+    </ol>
+
+    <h2>§3 Widerrufsrecht</h2>
+
+    <ol>
+
+      <li>
+        Der Teilnehmer hat nach §312g Abs. 2 (9) BGB kein Recht auf Widerruf des Kaufvertrags.
+      </li>
+
+      <li>
+        Der Kursgeber kann den Vertrag aus wichtigem Grund widerrufen. Einen wichtigen Grund stellt auch die zu geringe
+        Buchungszahl der Veranstaltung dar, durch die die Veranstaltung für den Kursgeber unwirtschaftlich wird.
+      </li>
+
+      <li>
+        Im Falle eines Vertragswiderrufs durch den Kursgeber nach §3 Abs. 2 steht dem Teilnehmer kein Aufwendungsersatz
+        zu.
+      </li>
+
+    </ol>
+
+    <h2>§4 Haftungsausschluss</h2>
+
+    <ol>
+
+      <li>
+        Jegliche Haftung durch Eric Feltgen auf Grund von nicht funktionstüchtigen Bestandteilen der Plattform ist
+        ausgeschlossen.
+      </li>
+
+      <li>
+        Jegliche Haftung durch Eric Feltgen auf Grund von Handlungen, Vorfällen, Leistungen oder Nicht-Leistungen im
+        Rahmen der Kurse ist ausgeschlossen.
+      </li>
+
+      <li>
+        Die einwandfreie technische Verfügbarkeit der Plattform kann nicht zu jedem Zeitpunkt gewährleistet werden.
+      </li>
+
+    </ol>
+
+    <h2>§5 Zahlungsabwicklung</h2>
+
+    <ol>
+
+      <li>
+        Kursgeber und Teilnehmer stimmen beim Abschluss des Kaufvertrags zu, dass die Zahlungsabwicklung durch die
+        Plattform übernommen wird.
+      </li>
+
+      <li>
+        Der Kursgeber erhält den Kaufpreis, abzüglich der Gebühren, erst nach Ablauf und Stattfinden der Veranstaltung.
+      </li>
+
+      <li>
+        Der Kursgeber erhält die Zahlung spätestens 30 Tage nach Stattfinden der Veranstaltung.
+      </li>
+
+      <li>
+        Sollte die Veranstaltung abgesagt und der Vertrag damit durch den Kursgeber nach §3 Abs. 2 widerrufen worden
+        sein, erhält der Teilnehmer seinen Kaufpreis zurück.
+      </li>
+
+    </ol>
+
+    <h2>§6 Gebühren</h2>
+
+    <ol>
+
+      <li>
+        Die Leistung der Plattform wird durch den Kursgeber bezahlt. Dies geschieht durch die in den folgenden
+        Paragraphen beschriebenen Gebühren.
+      </li>
+
+      <li>
+        Die Gebühren betragen 17% des Kaufpreises der Teilnahme an einer Veranstaltung. Diese werden bei der
+        Zahlungsabwicklung gemäß §5 Abs. 2 durch die Plattform einbehalten.
+      </li>
+
+      <li>
+        Im Falle des Widerrufs einer Veranstaltung durch den Kursgeber nach §3 Abs. 2 verpflichtet sich der Kursgeber
+        eine Bearbeitungsgebühr in Höhe von bis zu 0,35 EUR pro bereits gebuchte Teilnahme auf die entsprechende
+        Veranstaltung zu übernehmen.
+      </li>
+
+    </ol>
+
+    <h2>§7 Schlussbestimmungen</h2>
+
+    <ol>
+
+      <li>
+        Eric Feltgen kann den Nutzern jeder Zeit eine Änderung dieser AGB vorschlagen. Änderungen werden mindestens 30 Tage
+        vor dem vorgeschlagenen Zeitpunkt des Wirksamwerdens in Textform angeboten. Im Falle einer Nicht-Ablehnung vor
+        dem vorgeschlagenen Zeitpunkt wird die Änderung zum vorgeschlagenen Zeitpunkt wirksam.
+      </li>
+
+      <li>
+        Sollte ein Bestandteil der ABG nicht rechtskräftig sein, berührt das die Wirksamkeit der anderen Bestandteile
+        nicht.
+      </li>
+
+    </ol>
+
   </Layout>
 </template>
 <script>
@@ -10,3 +169,28 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+h1 {
+  @apply text-2xl font-bold text-text-gray mb-4;
+}
+
+h2 {
+  @apply text-xl font-bold text-text-gray mb-3;
+}
+
+h3 {
+  @apply text-lg font-bold text-text-gray mb-2;
+}
+
+p {
+  @apply mb-2;
+}
+
+ol {
+  @apply list-inside list-decimal mb-4;
+}
+
+li {
+  @apply mb-2;
+}
+</style>
