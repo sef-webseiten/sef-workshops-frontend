81d791270dd9cdd420e8ddcf462fb80c4d3d0dc2
EricHier <developer.erichier@gmail.com>
2021-03-06 14:32:05 +0100

    changing floating instagram size

--- src/components/gui-elements/FloatingInstagram.vue
+++ src/components/gui-elements/FloatingInstagram.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed bottom-0 right-0 w-screen md:w-fit p-6 z-20 text-text-gray">
-    <a class="bg-gray-50 rounded font-bold text-lg p-4 shadow-2xl flex flex-row items-center justify-start"
+    <a class="bg-gray-50 rounded font-bold text-lg p-4 py-2 shadow-2xl flex flex-row items-center justify-start"
        href="https://instagram.com/of.courz" rel="noopener" target="_blank">
       <fa :icon="['fab', 'instagram']" class="mr-4" size="2x"/>
       <span class="flex-grow text-center">Folge uns auf Instagram</span>
